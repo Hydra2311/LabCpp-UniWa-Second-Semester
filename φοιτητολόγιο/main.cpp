dbb7bfc8c829dedcafd39f9c64118b12b4afae1d
Hydra2311 <thodoris231107@gmail.com>
2026-06-05 14:44:55 +0300

    Variable

--- φοιτητολόγιο/main.cpp
+++ φοιτητολόγιο/main.cpp
@@ -14,14 +14,14 @@ int main()
     cout << "========================================" << endl;
     
     list<Subject> empty_list;
-    Teacher k1("Θοδωρής", 'M', "T01", "Action Dept", empty_list);
-    Teacher k2("Μιχάλης", 'M', "T02", "History Dept", empty_list);
-    Teacher k3("Thomas", 'M', "T03", "Programming Dept", empty_list);
+    Teacher Teacher1("Θοδωρής", 'M', "T01", "Action Dept", empty_list);
+    Teacher Teacher2("Μιχάλης", 'M', "T02", "History Dept", empty_list);
+    Teacher Teacher3("Thomas", 'M', "T03", "Programming Dept", empty_list);
 
-    Subject v1("848", "Action", 3, &k1), v2("373", "History", 4, &k2), v3(v2); 
-    v3.setSub("73653"); v3.setDes("Programming"); v3.setSem(3); v3.setHead(&k3); 
+    Subject sub1("848", "Action", 3, &Teacher1), sub2("373", "History", 4, &Teacher2), sub3(sub2); 
+    sub3.setSub("73653"); sub3.setDes("Programming"); sub3.setSem(3); sub3.setHead(&Teacher3); 
 
-    list<Subject> l = {v1, v2}; 
+    list<Subject> l = {sub1, sub2}; 
     Person Tommy("Thomas", 'M', l);
 
     cout << "\n========================================" << endl;
@@ -29,7 +29,7 @@ int main()
     cout << "========================================" << endl;
     Tommy.ListPrint(cout);
     
-    Tommy.AddSubject(v3);
+    Tommy.AddSubject(sub3);
     cout << "\n========================================" << endl;
     cout << "3. Πρόσθεση Subject" << endl;
     cout << "========================================" << endl;
@@ -60,7 +60,7 @@ int main()
     };
 
     class1A[0].SetMF('M'); 
-    class1A[1].AddSubject(v3);
+    class1A[1].AddSubject(sub3);
     for(int i = 0; i < n; i++) {class1A[i].Print(cout); cout << endl;}
 
     cout << endl;
@@ -93,22 +93,22 @@ int main()
     cout << "Λειτουργίες Record\n\n1. Αρχικοποίηση" << endl;
     cout << "========================================" << endl;
     
-    Record myRecord;
+    Record Φοιτητολογιο;
     list<Subject> emptyList;
 
     Teacher* t1 = new Teacher("Κύριος Γιώργος", 'M', "T01", "Μαθηματικά", emptyList);
     Teacher* t2 = new Teacher("Dr. Stein", 'M', "T02", "Γλώσσα", emptyList);
     Teacher* t3 = new Teacher("Κυρία Αναστασία", 'F', "T03", "Προγραμματισμός", emptyList);
-    myRecord.AddPerson(t1);
-    myRecord.AddPerson(t2);
-    myRecord.AddPerson(t3);
+    Φοιτητολογιο.AddPerson(t1);
+    Φοιτητολογιο.AddPerson(t2);
+    Φοιτητολογιο.AddPerson(t3);
 
     Subject math("101", "Algebra", 1, t1);
     Subject physics("102", "Physics", 2, t1);
     Subject python("103", "Python", 3, t2);
-    myRecord.AddSubject(math);
-    myRecord.AddSubject(physics);
-    myRecord.AddSubject(python);
+    Φοιτητολογιο.AddSubject(math);
+    Φοιτητολογιο.AddSubject(physics);
+    Φοιτητολογιο.AddSubject(python);
 
     Student* s1 = new Student("12345", "Thomas", 'M', 3, emptyList);
     Student* s2 = new Student("6789", "Νικολέτα", 'F', 3, emptyList);
@@ -123,19 +123,19 @@ int main()
     t2->AddSubject(physics);
     t3->AddSubject(python);
 
-    myRecord.AddPerson(s1);
-    myRecord.AddPerson(s2);
+    Φοιτητολογιο.AddPerson(s1);
+    Φοιτητολογιο.AddPerson(s2);
 
     cout << "--- Τωρινά Members ---" << endl;
-    myRecord.PrintMemb(cout);
+    Φοιτητολογιο.PrintMemb(cout);
 
 
     cout << "\n========================================" << endl;
     cout << "2. Αποθήκευση σε CSV" << endl;
     cout << "========================================" << endl;
     try {
-        myRecord.StudentOCSV();
-        myRecord.TeacherOCSV();
+        Φοιτητολογιο.StudentOCSV();
+        Φοιτητολογιο.TeacherOCSV();
         cout << "Δημιουργήθηκαν τα student.csv και teacher.csv" << endl;
     } 
     catch (int x) {
@@ -147,17 +147,17 @@ int main()
     cout << "3. Φόρτωση από CSV" << endl;
     cout << "========================================" << endl;
     
-    Record loadedRecord;
+    Record φορτωμενοφοιτ;
     
-    loadedRecord.AddSubject(math);
-    loadedRecord.AddSubject(physics);
-    loadedRecord.AddSubject(python);
+    φορτωμενοφοιτ.AddSubject(math);
+    φορτωμενοφοιτ.AddSubject(physics);
+    φορτωμενοφοιτ.AddSubject(python);
 
     try {
-        loadedRecord.StudentICSV();
-        loadedRecord.TeacherICSV();
+        φορτωμενοφοιτ.StudentICSV();
+        φορτωμενοφοιτ.TeacherICSV();
         cout << "Τα αρχεία φορτώθηκαν\n--- Τωρινά Members ---\n" << endl;
-        loadedRecord.PrintMemb(cout);
+        φορτωμενοφοιτ.PrintMemb(cout);
     }
     catch (int x) {
         cout << "Αδυναμία φόρτωσης" << endl;
@@ -168,26 +168,26 @@ int main()
     cout << "========================================" << endl;
 
     cout << "Thomas σε Tommy" << endl;
-    loadedRecord.ChangeName("12345", "Tommy");
+    φορτωμενοφοιτ.ChangeName("12345", "Tommy");
     
     cout << "Αλλαγή Subject 101 Description" << endl;
-    loadedRecord.ChangeDesc("101", "Advanced Algebra");
+    φορτωμενοφοιτ.ChangeDesc("101", "Advanced Algebra");
 
     cout << "--- Members μετά τις αλλαγές ---" << endl;
-    loadedRecord.PrintMemb(cout);
+    φορτωμενοφοιτ.PrintMemb(cout);
 
     cout << "\nΔιαγραφή κύριου Γιώργου" << endl;
-    loadedRecord.DeleteMember("T01");
+    φορτωμενοφοιτ.DeleteMember("T01");
     
     cout << "--- Members μετά τη διαγραφή ---" << endl;
-    loadedRecord.PrintMemb(cout);
+    φορτωμενοφοιτ.PrintMemb(cout);
 
     cout << "\n========================================" << endl;
     cout << "5. Δοκιμή Emails" << endl;
     cout << "========================================" << endl;
     
-    loadedRecord.EmailStudent(cout, "Ακύρωση του μαθήματος στις 17:00.");
-    loadedRecord.EmailTeacher(cout, "Συνάντηση καθηγητών στις 19:00");
+    φορτωμενοφοιτ.EmailStudent(cout, "Ακύρωση του μαθήματος στις 17:00.");
+    φορτωμενοφοιτ.EmailTeacher(cout, "Συνάντηση καθηγητών στις 19:00");
 
     cout << "\n========================================" << endl;
     cout << "Λειτουργίες Record ολοκληρώθηκαν" << endl;
